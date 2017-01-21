57b8ec23a6e4e0fcbfa80700266dbe4167f62fe6
Tyler Wong <ty-w@live.com>
2017-01-21 04:58:10 -0800

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# tyw_hashicorp_vault
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: tyw_hashicorp_vault
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
