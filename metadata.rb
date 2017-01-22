7728285e4a5d42f49dc69242bf7b6386b0f0bff6
Tyler Wong <ty-w@live.com>
2017-01-21 18:58:01 -0800

    Update description and bump major version

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,11 @@ name 'tyw_hashicorp_vault'
 maintainer 'Tyler Wong'
 maintainer_email 'ty-w@live.com'
 license 'all_rights'
-description 'Installs/Configures tyw_hashicorp_vault'
+description 'Wrapper cookbook for hashicorp-vault'
 long_description 'Installs/Configures tyw_hashicorp_vault'
 issues_url 'https://github.com/T-Wong/tyw_hashicorp_vault/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/T-Wong/tyw_hashicorp_vault' if respond_to?(:source_url)
-version '0.1.0'
+version '1.0.0'
 
 depends 'hashicorp-vault', '~> 2.4.0'
 depends 'magic_shell', '~> 1.0.0'
