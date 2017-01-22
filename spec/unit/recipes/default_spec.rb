8fcb7d862061d609a4965a11119e7104c6e50bdf
Tyler Wong <ty-w@live.com>
2017-01-21 18:55:57 -0800

    Add ChefSpec tests and rakefile

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -2,19 +2,28 @@
 # Cookbook:: tyw_hashicorp_vault
 # Spec:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, Tyler Wong, All Rights Reserved.
 
 require 'spec_helper'
 
 describe 'tyw_hashicorp_vault::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'includes hashicorp-vault' do
+      expect(chef_run).to include_recipe('hashicorp-vault::default')
+    end
+
+    it 'adds the required environment variables' do
+      expect(chef_run).to add_magic_shell_environment('PATH')
+      expect(chef_run).to add_magic_shell_environment('VAULT_ADDR')
     end
   end
 end
